662d65882b5b94b483293919d3fcb91f62a420a9
AlbertNoMes <albertnotariomestres@gmail.com>
2022-02-27 20:02:41 +0100

    falta commit final

---
+++ src/main/java/mainApp/Ud24SpringErEj2Application.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud24SpringErEj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud24SpringErEj2Application.class, args);
+	}
+
+}
